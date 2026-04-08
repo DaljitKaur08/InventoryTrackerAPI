bd1ffa5d6d4ae50003c4ae76496bd7492c791dc5
dj <saini26daljit@gmail.com>
2026-04-08 15:05:49 -0500

    Cleaned project files (.vs, bin)

--- InventoryTrackerAPI/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InventoryTrackerAPI.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-    }
-}
--- InventoryTrackerAPI/Models/Supplier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryTrackerAPI.Models
-{
-    public class Supplier
-    {
-        public int SupplierId { get; set; }
-        public string Name { get; set; }
-        public string ContactInfo { get; set; }
-    }
-}
